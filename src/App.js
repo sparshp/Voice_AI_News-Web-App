aa7c0f7cf732a4a1a1492864da0b05b46424bd34
sparshp <sparshprajapati9011334490@gmail.com>
2021-05-29 16:09:45 +0530

    Alan Voive Ai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+import wordsToNumbers from 'words-to-numbers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const alanKey= 'ee669c153a9e7812860cddeff634b1482e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () =>{
+    
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+
+    useEffect(() => {
+       alanBtn({
+           key: alanKey,
+           onCommand:({command, articles, number}) =>{
+               if(command ==='newHeadlines'){
+                   setNewsArticles(articles);
+                   setActiveArticle(-1);
+
+               } else if(command === 'highlight'){
+                   setActiveArticle((prevActiveArticle)=> prevActiveArticle +1);
+               } else if(command === 'open'){
+                   const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true}) : number ;
+                   const article = articles[parsedNumber -1];
+                   
+                   if(parsedNumber > 20) {
+                       alanBtn().playText('Please try that again.')
+                   } else if(article){
+                    window.open(article.url, '_blank');
+                    alanBtn().playText('Opening...');
+                   }
+               }
+           }
+       });
+    },[])
+
+
+    return(
+    <div> 
+      <div className={classes.logoContainer}>
+         <img src="https://westernnews.media.clients.ellingtoncms.com/img/photos/2018/08/07/Breaking_news_red.jpg" className={classes.alanLogo} alt="alan logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
-  );
+    );
 }
-
 export default App;
